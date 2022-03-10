bca3a0f487b40f48b9af9ac0eb054efd86aa3480
Thomas Delargy <tdelar@uw.edu>
2022-03-09 16:21:25 -0800

    the video version

--- components/SideBar.js
+++ components/SideBar.js
@@ -72,12 +72,6 @@ export default function Example(props) {
               {props.info?.p3}
             </dd>
           </div>
-          <div className="bg-gray-50 px-4 py-5 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6">
-            <dt className="text-sm font-medium text-gray-500">Median Income</dt>
-            <dd className="mt-1 text-sm text-gray-900 sm:mt-0 sm:col-span-2">
-              {props.info?.median_individual_income}
-            </dd>
-          </div>
 
           <div className="bg-white px-4 py-5 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6">
             <dt className="text-sm font-medium text-gray-500">
